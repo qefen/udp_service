f814a6243d81ee8c38810b5ed4e90f296e604768
kevin <kevinmartinez2107@gmail.com>
2020-09-18 10:11:37 -0500

    Depuracion de codigo

--- Pruebas Python/funcion.py
+++ Pruebas Python/funcion.py
@@ -1,4 +1,4 @@
-#Programa con una fucion que devuelve el numero mayor 
+#Programa con una fucion que devuelve el numero mayor de una lista de numeros indeterminada
 num =[10,15,60,8,1,30]
 
 def max(num):
@@ -10,13 +10,4 @@ def max(num):
         count=count+1
     return result
 
-         
-#for x in num:
-#    if num[count] > result:
-#        result=num[count]
-#    count=count+1
-
 print(max(num))
-
-
-
