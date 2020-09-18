63f41cfa05d77544be1c912581a5386d5283bf83
kevin <kevinmartinez2107@gmail.com>
2020-09-18 10:12:20 -0500

    Depuracion de codigo

--- Pruebas Python/bill_calculator.py
+++ Pruebas Python/bill_calculator.py
@@ -1,19 +1,42 @@
 #Bill Calculator 
 
 flag = True
-to_pay =0
-
-#flag = False
+to_pay = 0
 
 while flag:
-    unit = input ("Coloque la cantidad de unidades consumidas :")
-   
-    if int(unit)>100:
+    unit = int( str(input ("Coloque la cantidad de unidades consumidas : ")))
+    
+    if unit <=100:
         print("Cargo por las primeras 100 unidades")
         print("Rs.10/Unit")
-       
         to_pay=unit*10
-        print("TOTAL :"+str(to_pay))
+        print("TOTAL : "+str(to_pay))
+        flag = False
+
+    elif (unit<=200):
+        print("Cargo las 100 a 200 unidades")
+        print("Rs.15/Unit")
+        to_pay=(100*10)+(unit-100)*15
+        print("TOTAL : "+str(to_pay))
+        flag = False
+
+    elif(unit<=300):
+        print("Cargo las 200 a 250 unidades")
+        print("Rs.20/Unit")
+        to_pay=(100*10)+(100*15)+(unit-200)*20
+        print("TOTAL : "+str(to_pay))
+        flag = False
+    
+    elif(unit>300):
+        print("Cargo por encima de 300 unidades")
+        print("Rs.25/Unit")
+        to_pay=(100*10)+(100*15)+(100*20)+(unit-200)*20
+        print("TOTAL : "+str(to_pay))
+        flag = False
+
+            
+  
+        
         
 
    
